73bee3994a6334c22f13e5764f44de99b7febaae
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-07 18:17:31 +0300

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
+from os import environ
 from flask import Flask
 
 app = Flask(__name__)
 
 
+URL = 'https://api.telegram.org/bot{}/'.format(environ['STDY_TOKEN'])
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return URL
 
 
 if __name__ == '__main__':
