0b2bd381db8e0dbff65fdecc9b3e2294f8f42952
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-07 19:11:33 +0300

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from os import environ
+from os import getenv
 from flask import Flask
 
 app = Flask(__name__)
 
 
-URL = 'https://api.telegram.org/bot{}/'.format(environ['STDY_TOKEN'])
+URL = 'https://api.telegram.org/bot{}/'.format(getenv('STDY_TOKEN'))
 
 
 @app.route('/')
