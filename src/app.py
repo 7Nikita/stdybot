c4352cafc939f47c6963c26e4884b56145e54fa2
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-08 18:12:50 +0300

    added send_message method

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import utility
+import requests
 from os import getenv
 from flask import Flask
 from flask import request
@@ -12,13 +13,20 @@ STDY_TOKEN = getenv('STDY_TOKEN')
 URL = 'https://api.telegram.org/bot{}/'.format(STDY_TOKEN)
 
 
-@app.route('/{}'.format(STDY_TOKEN), methods=['GET', 'POST'])
+@app.route('/{}'.format(STDY_TOKEN), methods=['POST', 'GET'])
 def webhook():
     if request.method == 'POST':
         r = request.get_json()
         utility.write_json(r)
         return jsonify(r)
     return '<h1>Bot start page</h1>'
+
+
+def send_message(chat_id, text):
+    url = URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url, json=answer)
+    return r.json()
 
 
 if __name__ == '__main__':
