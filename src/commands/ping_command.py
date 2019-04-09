ab1eaf0806fde64b7e8264913c927320910f17b9
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-09 15:36:47 +0300

    added ping command

---
+++ src/commands/ping_command.py
@@ -0,0 +1,13 @@
+import time
+import command_system
+
+
+def get_ping(request):
+    return "pong ({0} ms)".format(int(1000 * (time.time() - request['message']['date'])))
+
+
+ping_command = command_system.Command()
+
+ping_command.keys = [r"/ping"]
+ping_command.description = "get ping"
+ping_command.process = get_ping
