e8a248c1db0ca24d114553acf993f29816368b2a
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-20 17:48:56 +0300

    added today command

---
+++ src/commands/today_command.py
@@ -0,0 +1,24 @@
+import re
+import app
+import schedule_tools
+import command_system
+
+
+def today_schedule(request):
+
+    group = re.findall(r'\d+', request['message']['text'])[-1]
+
+    if not app.db['groups'].count_documents({'name': group}):
+        return 'Invalid group number'
+
+    cur_day = app.db['info'].find_one({'name': 'current_day'})['day']
+    cur_week = app.db['info'].find_one({'name': 'current_week'})['week']
+
+    return schedule_tools.get_schedule(group, cur_day, cur_week)
+
+
+today_schedule_command = command_system.Command()
+
+today_schedule_command.keys = [r'/today \d+\t*']
+today_schedule_command.description = 'getting today schedule by group'
+today_schedule_command.process = today_schedule
