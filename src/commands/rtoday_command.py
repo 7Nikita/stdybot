64682ae194d787fb33fe9999a5dd7e30455a02e8
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-21 17:13:34 +0300

    added commands for registered users

---
+++ src/commands/rtoday_command.py
@@ -0,0 +1,21 @@
+import app
+import schedule_tools
+import command_system
+
+
+def rtoday_schedule(request):
+
+    user_id = request['message']['from']['id']
+    group = app.db['users'].find_one({'name': user_id})['group']
+
+    cur_day = app.db['info'].find_one({'name': 'current_day'})['day']
+    cur_week = app.db['info'].find_one({'name': 'current_week'})['week']
+
+    return schedule_tools.get_schedule(group, cur_day, cur_week)
+
+
+rtoday_schedule_command = command_system.Command()
+
+rtoday_schedule_command.keys = [r'/today\t*']
+rtoday_schedule_command.description = 'getting today schedule by your group'
+rtoday_schedule_command.process = rtoday_schedule
---
+++ src/commands/rtomorrow_command.py
@@ -0,0 +1,21 @@
+import app
+import schedule_tools
+import command_system
+
+
+def rtomorrow_schedule(request):
+
+    user_id = request['message']['from']['id']
+    group = app.db['users'].find_one({'name': user_id})['group']
+
+    cur_day = (app.db['info'].find_one({'name': 'current_day'})['day'] + 1) % 7
+    cur_week = (app.db['info'].find_one({'name': 'current_week'})['week'] + cur_day == 0) % 4 + 1
+
+    return schedule_tools.get_schedule(group, cur_day, cur_week)
+
+
+rtomorrow_schedule_command = command_system.Command()
+
+rtomorrow_schedule_command.keys = [r'/tomorrow\t*']
+rtomorrow_schedule_command.description = 'getting tomorrow schedule by your group'
+rtomorrow_schedule_command.process = rtomorrow_schedule
