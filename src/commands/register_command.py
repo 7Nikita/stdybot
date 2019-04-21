0bbca65f67d0c96fb49f5e55e1e1b42261c9f279
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-21 17:12:55 +0300

    added registration

---
+++ src/commands/register_command.py
@@ -0,0 +1,23 @@
+import re
+import app
+import command_system
+
+
+def register(request):
+
+    user_id = request['message']['from']['id']
+    group = re.findall(r'\d+', request['message']['text'])[-1]
+
+    if not app.db['groups'].count_documents({'name': group}):
+        return 'Invalid group number'
+
+    app.db['users'].replace_one({'name': user_id}, {'name': user_id, 'group': group}, upsert=True)
+
+    return 'registered'
+
+
+register_command = command_system.Command()
+
+register_command.keys = [r'/reg \d+\t*']
+register_command.description = 'register your university group'
+register_command.process = register
