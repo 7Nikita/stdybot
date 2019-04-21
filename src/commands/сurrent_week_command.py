1fdaadfc5e54d69545168d26056864dc53650619
Nikita Pekurin <nikita.mda@yandex.by>
2019-04-21 18:10:21 +0300

    added current week command

---
+++ src/commands/сurrent_week_command.py
@@ -0,0 +1,13 @@
+import app
+import command_system
+
+
+def get_week(request):
+    return app.db['info'].find_one({'name': 'current_week'})['week']
+
+
+get_week_command = command_system.Command()
+
+get_week_command.keys = [r'/week']
+get_week_command.description = 'get week'
+get_week_command.process = get_week
