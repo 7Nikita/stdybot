13e358478851c914909fc54a83d007d2d824af13
Nikita Pekurin <nikita.mda@yandex.by>
2019-05-16 16:35:07 +0300

    added help

---
+++ src/commands/help_command.py
@@ -0,0 +1,15 @@
+import command_system
+
+
+def help(request):
+    ans = ""
+    for command in command_system:
+        ans += "{}: {}\n".format(command.keys[-1], command.description)
+    return ans
+
+
+help_command = command_system.Command()
+
+help_command.keys = [r'/start\s*', r'/help\s*']
+help_command.description = 'print help'
+help_command.process = help
